892b93bc237ca0e7b9ae19291eb763be9369bfca
Rafaela_Viana <rafaelacorreaviana@gmail.com>
2026-09-04 08:53:45 +0100

    Ficheiro.txt com o link do GitHub

--- AdotaPatas/AdotaPatas/Login.Designer.cs
+++ AdotaPatas/AdotaPatas/Login.Designer.cs
@@ -47,7 +47,7 @@
             this.textBox1.Location = new System.Drawing.Point(839, 171);
             this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(397, 37);
+            this.textBox1.Size = new System.Drawing.Size(397, 27);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
@@ -58,7 +58,7 @@
             this.textBox2.Location = new System.Drawing.Point(839, 265);
             this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(397, 37);
+            this.textBox2.Size = new System.Drawing.Size(397, 27);
             this.textBox2.TabIndex = 1;
             // 
             // label1
@@ -70,7 +70,7 @@
             this.label1.Location = new System.Drawing.Point(743, 174);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 30);
+            this.label1.Size = new System.Drawing.Size(49, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Email:";
             // 
@@ -83,7 +83,7 @@
             this.label2.Location = new System.Drawing.Point(704, 272);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 30);
+            this.label2.Size = new System.Drawing.Size(73, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Password:";
             // 
@@ -124,7 +124,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(878, 525);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(166, 30);
+            this.label3.Size = new System.Drawing.Size(115, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Não tem conta?";
             // 
@@ -139,7 +139,7 @@
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(192)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1316, 706);
